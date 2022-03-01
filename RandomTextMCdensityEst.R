cc14f1069e32e6a2152894c2f65a4cb2a7e86e45
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-03-01 21:38:10 +0100

    Add files via upload

---
+++ RandomTextMCdensityEst.R
@@ -0,0 +1,19 @@
+print('file: RandomTentMCdensityEst.R')
+# estimate density from 1e5 random numbers from tent PDF
+set.seed(1953)
+M = 1e5; r = runif(M);  # uniformly distributed random numbers
+y = numeric(M)
+for (k in 1:M) {    # transformation y(x)
+  x = r[k];
+  if (x < 0.5) {y[k] = sqrt(x/2)}
+  else {y[k] = 1-sqrt((1-x)/2)}}
+# png('tentDensityEstimate160722.png',width=16,height=12,units='cm',res=300)
+plot(density(y,from=0.0,to=1.0),type='l',lwd=3,col='blue',
+     xlab='y',ylab='Density',main='',las=1,xlim=c(0,1),ylim=c(0,2),cex.lab=1.5)
+dy = 0.01; y1 = seq(0,0.5,dy); y2 = seq(0.5,1,dy)
+q1 = 4*y1; q2 = 4*(1-y2)
+lines(y1,q1,lwd=1,col='red',lty=4)
+lines(y2,q2,lwd=1,col='red',lty=4)
+legend('topright',legend=c('MC estimate','tent PDF'),col=c('blue','red'),
+       lty=c(1,4),lwd=c(3,1))
+# dev.off()
