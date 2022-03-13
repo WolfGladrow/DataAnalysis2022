64fe325054def9d62ffae9f95104aab234dccc0f
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-03-13 07:21:42 +0100

    Add files via upload

---
+++ VarRatioTestCountData.R
@@ -0,0 +1,9 @@
+print('file: VarRatioTestCountData.R')
+# apply variance ratio test to count data fromZar (2010, Example 8.7)
+x1 = c(41,35,33,36,40,46,31,37,34,30,38) # number of moths in trap #1
+x2 = c(52,57,62,55,64,57,56,55,60,59)    # number of moths in trap #2
+out3 = var.test(x1,x2); p = out3$p.value; print(c(round(p,4),'p'))
+out3a = var.test(x2,x1); p2 = out3a$p.value; print(c(round(p2,4),'p2'))
+alpha = 0.05
+if(p < alpha) print(c('p < alpha = ',alpha,': reject H0'))
+if(p >= alpha) print(c('p >= alpha = ',alpha,': do not reject H0'))
