94f5be1783efe44a1b583356b726a17457a1b66f
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-03-30 14:34:18 +0200

    Add files via upload

--- PDsPDFsNormalDifferentMeans.R
+++ PDsPDFsNormalDifferentMeans.R
@@ -1,9 +1,10 @@
 print('file: PDsPDFsNormalDifferentMeans.R')
 # normal distributions with different means
 library(latex2exp)
-# png('NormalDistDiffMeanBook220225.png',width=16,height=12,units='cm',res=300)
+# png('NormalDistDiffMeanBook220225.png',width=16,height=16,units='cm',res=300)
 x = seq(-4,4,0.01); N = dnorm(x); Nm1 = dnorm(x,-1); Np1 = dnorm(x,1)
-plot(x,N,type='l',xlab='x',ylab='Normal distributions',col='blue',lwd=3,xlim = c(-4,4),ylim=c(0,0.48),las=1) 
+plot(x,N,type='l',xlab='x',ylab='Normal distributions',col='blue',lwd=3,
+     xlim = c(-4,4),ylim=c(0,0.48),las=1,cex.lab=1.5) 
 lines(x,Nm1,col='black',lwd=3,lty=2)
 lines(x,Np1,col='red',lwd=3,lty=3)
 yt = 0.44
