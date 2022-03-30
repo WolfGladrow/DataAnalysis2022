9be2bfae04f05bb7b04ed87dd7e98861b2019c07
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-03-30 10:49:23 +0200

    Add files via upload

---
+++ PDsPDFsPoissonPDexample.R
@@ -0,0 +1,8 @@
+print('file: PDsPDFsPoissonPDexample.R')
+# Poisson distribution (example)
+lambda1 = 2.7; k = seq(0,10); pk = dpois(k,lambda1) 
+# png('PoissonPD160817.png',width=16,height=12,units='cm',res=300)
+plot(k,pk,type='p',lwd=4,col='blue',xlab='k',ylab='Probability',las=1,
+     cex=0.6,cex.lab=1.5)
+text(7,0.2,bquote(~lambda == .(lambda1)),col='blue',cex=1.5)
+# dev.off()
