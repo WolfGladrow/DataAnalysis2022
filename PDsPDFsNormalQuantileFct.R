c7923335483ba8ca3b891b559fdf09dcdb8affb4
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-03-30 14:51:30 +0200

    Add files via upload

--- PDsPDFsNormalQuantileFct.R
+++ PDsPDFsNormalQuantileFct.R
@@ -2,6 +2,6 @@ print('file: PDsPDFsNormalQuantileFct.R')
 # quantile function for standard normal PDF
 p = seq(0.001,0.999,0.001)
 x = qnorm(p)
-# png('QuantileStdNormal190620.png',width=16,height=12,units='cm',res=300)
-plot(p,x,type='l',lwd=3,col='blue',xlab='p',ylab='x',las=1,cex=0.4)
+# png('QuantileStdNormal190620.png',width=16,height=16,units='cm',res=300)
+plot(p,x,type='l',lwd=3,col='blue',xlab='p',ylab='x',las=1,cex.lab=1.5)
 # dev.off()
