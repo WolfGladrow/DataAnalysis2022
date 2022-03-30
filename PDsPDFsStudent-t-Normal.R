321233c48264364f6429a33f49332aef916aa631
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-03-30 20:31:06 +0200

    Add files via upload

--- PDsPDFsStudent-t-Normal.R
+++ PDsPDFsStudent-t-Normal.R
@@ -3,10 +3,9 @@ print('file: PDsPDFsStudent-t-Normal.R')
 t = seq(-3,3,0.01); N = dnorm(t)
 t3 = dt(t,3); t5 = dt(t,5); t10 = dt(t,10)
 library(latex2exp)
-# png('tDistNormalBook160103R.png',width=16,height=12,units='cm',res=300)
-plot(t,t3,type='l',col='blue',xlab='t',
-     ylab='Standard normal and t distributions',
-     xlim=c(-3,3), ylim=c(0,0.45),lwd=3,las=1,lty=4)
+# png('tDistNormalBook160103R.png',width=16,height=16,units='cm',res=300)
+plot(t,t3,type='l',col='blue',xlab='t',ylab='Density',
+     xlim=c(-3,3), ylim=c(0,0.45),lwd=3,las=1,lty=4,cex.lab=1.5)
 lines(t,t5,col='red',lwd=2,lty=2)
 lines(t,t10,col='black',lwd=2,lty=3)
 lines(t,N,col='magenta',lwd=3,lty=1)
