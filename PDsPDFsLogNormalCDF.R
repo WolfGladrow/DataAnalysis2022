bd8c80c2520b0fe3d196cb78162a45bf061280f2
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-03-31 22:17:48 +0200

    Add files via upload

--- PDsPDFsLogNormalCDF.R
+++ PDsPDFsLogNormalCDF.R
@@ -1,7 +1,6 @@
 print('file: PDsPDFsLogNormalCDF.R')
 # CDF of standard log-normal distribution
 x = seq(0,5,0.01); CDFLN = plnorm(x)
-# png('LogNormalCDFStdBook160405.png',width=16,height=12,units='cm',res=300)
-plot(x,CDFLN,type='l',xlab='x',ylab='CDF of log-normal distribution',
-     ylim=c(0,1),col='blue',lwd=3,las=1,cex.lab=1.5) 
+# png('LogNormalCDFStdBook160405.png',width=16,height=16,units='cm',res=300)
+plot(x,CDFLN,type='l',xlab='x',ylab='CDF',ylim=c(0,1),col='blue',lwd=4,las=1,cex.lab=1.5) 
 # dev.off()
--- PDsPDFsLogNormalPDF.R
+++ PDsPDFsLogNormalPDF.R
@@ -1,7 +1,6 @@
 print('file: PDsPDFsLogNormalPDF.R')
 # standard log-normal distribution
 x = seq(0,3,0.01); LN = dlnorm(x)
-# png('LogNormalPDFStdBook160405.png',width=16,height=12,units='cm',res=300)
-plot(x,LN,type='l',xlab='x',ylab='Log-normal distribution, standard',
-     col='blue',lwd=3,las=1,cex.lab=1.5) 
+# png('LogNormalPDFStdBook160405.png',width=16,height=16,units='cm',res=300)
+plot(x,LN,type='l',xlab='x',ylab='Density',col='blue',lwd=4,las=1,cex.lab=1.5) 
 # dev.off()
