510d5fcac8b192afc12d8f2580da49d11d83c57f
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-03-31 21:55:30 +0200

    Add files via upload

--- PDsPDFsInvChiSquaredCDF.R
+++ PDsPDFsInvChiSquaredCDF.R
@@ -3,8 +3,8 @@ print('file: PDsPDFsInvChiSquaredCDF.R')
 invChiSqCDF = function(x,nu) {pgamma(nu/2,1/(2*x))}
 x = seq(0.01,1,0.001)
 chisq3 = invChiSqCDF(x,3); chisq5 = invChiSqCDF(x,5); chisq10 = invChiSqCDF(x,10)
-# png('invchi-squaredCDFBook160104R.png',width=16,height=12,units='cm',res=300)
-plot(x,chisq3,type='l',col='blue',xlab='t',ylab='Chi-squared distributions',lwd=3,
+# png('invchi-squaredCDFBook160104R.png',width=16,height=16,units='cm',res=300)
+plot(x,chisq3,type='l',col='blue',xlab='t',ylab='CDF',lwd=4,
      ylim=c(0,1),las=1,cex.lab=1.5)
 lines(x,chisq5,col='red',lwd=3,lty=2); lines(x,chisq10,col='black',lwd=3,lty=3)
 xt = 0.5
