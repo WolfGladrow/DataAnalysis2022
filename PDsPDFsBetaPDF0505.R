89cc1ba53728ef0cb4995683418b934b27f5e9bd
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-01 08:08:02 +0200

    Add files via upload

--- PDsPDFsBetaPDF0505.R
+++ PDsPDFsBetaPDF0505.R
@@ -3,9 +3,9 @@ print('file: PDsPDFsBetaPDF0505.R')
 alpha = 0.5; beta = 0.5
 xarr = seq(0.001,0.999,0.001)
 betaPDF = dbeta(x=xarr,shape1=alpha,shape2=beta)
-# png('beta0505PDF191017.png',width=16,height=12,units='cm',res=300)
+# png('beta0505PDF191017.png',width=16,height=16,units='cm',res=300)
 plot(xarr,betaPDF,type='l',lwd=3,col='blue',xlab='x',
-     ylab='Beta PDF',las=1,ylim=c(0,max(betaPDF)),cex.lab=1.5)
-text(0.5,8,expression(paste(alpha,' = 0.5')),col='blue',cex=1.5)
-text(0.5,6,expression(paste(beta,' = 0.5')),col='blue',cex=1.5)
+     ylab='Density',las=1,ylim=c(0,max(betaPDF)),cex.lab=1.5)
+text(0.5,8,bquote(~alpha == .(alpha)),col='blue',cex=1.5)
+text(0.5,6,bquote(~beta == .(beta)),col='blue',cex=1.5)
 # dev.off()
