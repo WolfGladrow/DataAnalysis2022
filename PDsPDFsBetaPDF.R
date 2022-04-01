3a6d2440a8c6aec58d3afa31e8e2543c5d41af2d
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-01 07:56:02 +0200

    Add files via upload

--- PDsPDFsBetaPDF.R
+++ PDsPDFsBetaPDF.R
@@ -3,9 +3,9 @@ print('file: PDsPDFsBetaPDF.R')
 alpha = 2; beta = 1.5
 parr = seq(0.0,1,0.001)
 betaPDF = dbeta(x=parr,shape1=alpha,shape2=beta)
-# png('betaPDF160818.png',width=16,height=12,units='cm',res=300)
+# png('betaPDF160818.png',width=16,height=16,units='cm',res=300)
 plot(parr,betaPDF,type='l',lwd=3,col='blue',xlab='p',
-     ylab='Beta PDF',las=1,cex.lab=1.5)
-text(0.5,0.8,expression(paste(alpha,' = 2')),col='blue',cex=1.5)
-text(0.5,0.6,expression(paste(beta,' = 1.5')),col='blue',cex=1.5)
+     ylab='Density',las=1,cex.lab=1.5)
+text(0.5,0.8,bquote(~alpha == .(alpha)),col='blue',cex=1.5)
+text(0.5,0.6,bquote(~beta == .(beta)),col='blue',cex=1.5)
 # dev.off()
