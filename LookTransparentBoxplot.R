6eee855d8f628295735cb93e6d52dbeaa9d82dff
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-02 20:47:17 +0200

    Add files via upload

--- LookTransparentBoxplot.R
+++ LookTransparentBoxplot.R
@@ -6,9 +6,9 @@ x1 = c(-0.68644, -0.82379, -0.98416, -2.02230, -0.43507, -0.76655,
        -1.29916, -0.30322, -0.77935, -0.97190, 0.84580, 0.28698,
        1.15160, 0.35533, 0.32936, 1.68584, 0.18260, 1.93600)
 library(latex2exp)
-# png('TransparentBoxplot220220.png',width=16,height=12,units='cm',res=300)
+# png('TransparentBoxplot220220.png',width=16,height=16,units='cm',res=300)
 x1[1] = 5 # create an 'outlier'
-boxplot(x1,col='yellow',las=1,xlab=TeX('$x_1$')) 
+boxplot(x1,col='yellow',las=1,xlab=TeX('$x_1$'),cex.lab=1.5) 
 L = length(x1)
 set.seed(1953) # set seed for random number generators
 jitter = runif(L,1-0.05,1+0.05)
