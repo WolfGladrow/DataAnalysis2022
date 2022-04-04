0befca1a8e2464f0a0aec26a0fb6bd1206b032e2
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-04 21:28:45 +0200

    Add files via upload

---
+++ ProbBernoulliDice6n.R
@@ -0,0 +1,14 @@
+print('file: ProbBernoulliDice6n.R')
+# rolling the dice
+p = 1/6
+n = 6
+print(c(n,'n'))
+k = seq(0,n)
+B6 = dbinom(k,n,p)
+print(c(round(B6,6),'B(k;n=6,p=1/6)'))
+print(c(round(1/B6[7],1),'1/B(6;n=6,p=1/6)'))
+# png('BernoulliDice6n200515.png',width=16,height=16,units='cm',res=300)
+plot(k,B6,type='p',lwd=4,col='blue',xlab='k',ylab=NA,las=1,cex=0.6,cex.lab=1.5)
+title(ylab='B(k;n=6,p=1/6)',line=2.5,cex.lab=1.5)
+# dev.off()
+
