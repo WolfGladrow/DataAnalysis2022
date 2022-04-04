854c1c1261338704976b7cd4b416182b9437c8eb
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-04 16:55:14 +0200

    Add files via upload

--- MVCCexampleCovSanudo.R
+++ MVCCexampleCovSanudo.R
@@ -6,9 +6,9 @@ print(c(round(cov(x,y),2),'covariance(x,y)'))
 print(c(round(cor(x,y),2),'correlation(x,y)'))
 r = cor(x,y); print(c(round(r,4),'r Pearson correlation coefficient'))   # -0.4543169
 library(latex2exp)
-# png('Sanudo06a220222b.png',width=16,height=12,units='cm',res=300)
-plot(x,y,type='p',col='blue',lwd=3,cex=0.4,ylim=c(0,80),las=1,
-     xlab=TeX('$\\[ PO_4 \\]\\, (\\mu mol\\, L^{-1})$'),ylab=NA)
-title(ylab=TeX('$Vitamin\\, B_{12}\\, (pmol\\, L^{-1})$'),line=2)
-text(1,70,paste('r = ',as.character(round(r,2))),col='red')
+# png('Sanudo06a220222b.png',width=16,height=16,units='cm',res=300)
+plot(x,y,type='p',col='blue',lwd=4,cex=0.6,ylim=c(0,80),las=1,
+     xlab=TeX('$\\[ PO_4 \\]\\, (\\mu mol\\, L^{-1})$'),ylab=NA,cex.lab=1.5)
+title(ylab=TeX('$Vitamin\\, B_{12}\\, (pmol\\, L^{-1})$'),line=2.3,cex.lab=1.5)
+text(1,70,paste('r = ',as.character(round(r,2))),col='black',cex=1.5)
 # dev.off()
