06ee2c9fce6c9f16d6b85cf6ca8732d1b6fd361a
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-05 13:20:39 +0200

    Add files via upload

--- PDsPDFsUniformDiscrete6.R
+++ PDsPDFsUniformDiscrete6.R
@@ -1,7 +1,7 @@
 print('file: PDsPDFsUniformDiscrete6.R')
 # plot discrete uniform distribution for die
 N = 6; xp = seq(1,N); yp =numeric(N) + 1/N
-# png('UnifDiscrete160817.png',width=16,height=12,units='cm',res=300)
+# png('UnifDiscrete160817.png',width=16,height=16,units='cm',res=300)
 plot(xp,yp,type='p',lwd=4,col='blue',xlab='k',
      ylab='Probability',las=1,cex=0.6,ylim=c(0,0.2))
 # dev.off()
