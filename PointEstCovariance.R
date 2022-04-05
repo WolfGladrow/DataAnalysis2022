90882832cda157c752da09b84ca43829620537de
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-05 22:34:43 +0200

    Add files via upload

--- PointEstCovariance.R
+++ PointEstCovariance.R
@@ -18,10 +18,10 @@ out = sampleUcorFct(r,nLarge)
 xL = out[1:nLarge]; yL = out[(nLarge+1):(2*nLarge)]
 print(c(round(cor(xL,yL),4),'correlation'))
 print(c(round(cov(xL,yL),4),'covariance'))
-# png('CovarianceEst200707.png',width=16,height=12,units='cm',res=300)
-plot(narr,covEst1,log='x',type='p',lwd=3,col='blue',xlab='log(n)',
-       ylab='Covariance, estimated',las=1,cex=0.4,
-       ylim=c(min(covEst1,covEst1)-0.01,0.01+max(covEst1,covEst1)),cex.lab=1.5)
-  points(narr,covEst2,col='red',pch=24,lwd=3,cex=0.4)
-  abline(h=0.0417,col='black',lty=2)
+# png('CovarianceEst200707.png',width=16,height=16,units='cm',res=300)
+plot(narr,covEst1,log='x',type='p',lwd=4,col='blue',xlab='log(n)',
+     ylab='Covariance',las=1,cex=0.6,
+     ylim=c(min(covEst1,covEst1)-0.01,0.01+max(covEst1,covEst1)),cex.lab=1.5)
+points(narr,covEst2,col='magenta',pch=24,lwd=4,cex=0.6)
+abline(h=0.0417,col='black',lty=2)
 # dev.off()
