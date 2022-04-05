eb7f1dfdc37e6ccf7d71a5aaa1a4a646a4b87ea1
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-05 14:50:19 +0200

    Add files via upload

--- PDsPDFsNormalStandard.R
+++ PDsPDFsNormalStandard.R
@@ -2,8 +2,9 @@ print('file: PDsPDFsNormalStandard.R')
 # standard normal distribution 
 x = seq(-3,3,0.01); y = dnorm(x)
 library(latex2exp)
-# png('NormalDistStdBook220225.png',width=16,height=12,units='cm',res=300)
-plot(x,y,type='l',xlab='x',ylab='Standard normal distribution',col='blue',lwd=3,las=1) 
+# png('NormalDistStdBook220225.png',width=16,height=16,units='cm',res=300)
+plot(x,y,type='l',xlab='x',ylab='Standard normal distribution',col='blue',lwd=3,
+     las=1,cex.lab=1.5) 
 text(0,0.15,TeX('$\\mu = 0$'),col='blue',cex=2)
 text(0,0.1,TeX('$\\sigma = 1$'),col='blue',cex=2)
 # dev.off()
