185b5fffdfabda19bb450e1e826299335d12164c
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-07 10:33:09 +0200

    Add files via upload

--- NHST-Mendel-Fisher.R
+++ NHST-Mendel-Fisher.R
@@ -12,7 +12,7 @@ chisqc = qchisq(p=(1-0.05),df=nu);
 print(c(round(chisqc,2),' critical chi-square value for alpha=0.05'))
 p = (1-pchisq(q=totalq,df=nu)); print(c(round(p,4),' p-value'))
 xp = seq(1,4)
-# png('MendelianExercise190415a.png',width=16,height=12,units='cm',res=300)
+# png('MendelianExercise190415a.png',width=16,height=16,units='cm',res=300)
 plot(xp,fobs,type='p',lwd=4,col='blue',xlab='Sample',ylab='# of individuals',
      las=1,cex=0.6,ylim=c(0,350),cex.lab=1.5)
 points(xp,fexp,col='magenta',lwd=4,pch=24,cex=0.6)
