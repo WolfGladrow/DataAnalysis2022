d07d1967195df27c4feffb04ebea4a448d86c2bd
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-07 20:04:41 +0200

    Add files via upload

--- MC-F-PDFs-VRTnormal.R
+++ MC-F-PDFs-VRTnormal.R
@@ -11,10 +11,10 @@ out = density(Fstat.vector,from=0,to=3)  # note: specify the range for density e
 xd = out$x; yd = out$y; yF = df(xd,df1=m-1,df2=n-1); d = yF-yd
 sflag = 1
 if(sflag == 1) {
-  # png('VartestuniformPDnorm170704.png',width=16,height=12,units='cm',res=300)
-  plot(xd,yd,type='l',xlim=c(0,3),ylim=c(-0.1,0.8),lwd=3,main='',xlab='F',
+  # png('VartestuniformPDnorm170704.png',width=16,height=16,units='cm',res=300)
+  plot(xd,yd,type='l',xlim=c(0,3),ylim=c(-0.1,0.8),lwd=4,main='',xlab='F',
        ylab='Density',col='black',cex.lab=1.5)
-  lines(xd,yF,col='magenta',lwd=3,lty=4)
+  lines(xd,yF,col='magenta',lwd=4,lty=4)
   lines(xd,d,col='blue',lty=1)
   legend('topright',c('Monte Carlo','F(x;10,9)','difference'),lwd=c(3,3,1),
          col=c('black','magenta','blue'),lty=c(1,4,1))
