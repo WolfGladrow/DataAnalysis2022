c8f92ff87e9425b2f644e270aa9a3e64642ac905
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-08 23:01:35 +0200

    Add files via upload

---
+++ RNasymTriangle.R
@@ -0,0 +1,29 @@
+print('file: RNasymTriangle.R')
+# random numbers from asymmetric triangular PDF: mode = 0.9 
+# install.packages('triangle')
+library(triangle)
+set.seed(1953) 
+thetaMode = 0.9 # true mode
+sflag = 1
+if (sflag == 1) {
+M = 1e3 # number of Monte Carlo runs
+x = rtriangle(M,0,1,thetaMode)
+# png('TriPDFnonsym0d9M1e3R210614.png',width=16,height=16,units='cm',res=300)   # 1e3
+plot(density(x,from=0,to=1),type='l',lwd=3,col='blue',xlab='x',
+     ylab='Density',las=1,cex.lab=1.5,main='',ylim=c(0,2))
+text(0.2,1.5,paste('M = ',as.character(M)),col='blue',pos=4,cex=1.5)
+dx = 0.001; xp = seq(dx,1-dx,dx); yp = nonsymtri(xp,thetaMode)
+lines(xp,yp,col='black')
+# dev.off()
+}
+if (sflag == 2) {
+M = 1e5
+x = rtriangle(M,0,1,thetaMode)
+# png('TriPDFnonsym0d9M1e5R210614.png',width=16,height=12,units='cm',res=300)   # 1e5
+plot(density(x,from=0,to=1),type='l',lwd=3,col='blue',xlab='x',
+     ylab='Density',las=1,cex.lab=1.5,main='',ylim=c(0,2))
+text(0.2,1.5,paste('M = ',as.character(M)),col='blue',pos=4,cex=1.5)
+dx = 0.001; xp = seq(dx,1-dx,dx); yp = nonsymtri(xp,thetaMode)
+lines(xp,yp,col='black')
+# dev.off()
+}
