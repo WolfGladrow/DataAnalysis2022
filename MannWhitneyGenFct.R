2fc64f08be7cd748c1578ef29478e9ae8170c883
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-08 13:13:39 +0200

    Add files via upload

--- MannWhitneyGenFct.R
+++ MannWhitneyGenFct.R
@@ -5,10 +5,7 @@ fx = function(x) { # (1) generating function
   s1 = 1; s2 = 1; for(i in (m+1):(m+n)) s1=s1*(1-x^i); for(i in 1:n) s2=s2*(1-x^i);
   return(s1/s2/(factorial(n+m)/factorial(m)/factorial(n)))}
 xa = seq(-0.5,0.5,1e-3)
-sflag = 1
-if (sflag == 1) {
-  # png('GenFct33MWtest180527.png',width=16,height=12,units='cm',res=300)
-  plot(xa,fx(xa),type='l',lwd=3,col='blue',xlab='x',
-       ylab='Generating function f(x;3,3)',las=1,cex=0.4,cex.lab=1.5)
-  # dev.off()
-}
+# png('GenFct33MWtest180527.png',width=16,height=16,units='cm',res=300)
+plot(xa,fx(xa),type='l',lwd=4,col='blue',xlab='x',
+       ylab='Generating function f(x;3,3)',las=1,cex.lab=1.5)
+# dev.off()
