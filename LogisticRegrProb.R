cdd35b242590fe96ebc96c3a3784a8fca8a0fec4
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-09 18:32:48 +0200

    Add files via upload

--- LogisticRegrProb.R
+++ LogisticRegrProb.R
@@ -9,14 +9,14 @@ p = 1/(1+exp(-eta)) # probability of success
 library(latex2exp)
 sflag = 1
 if(sflag == 1) {
-  # png('LogRegEta161001.png',width=16,height=12,units='cm',res=300)
+  # png('LogRegEta161001.png',width=16,height=16,units='cm',res=300)
   plot(x,eta,type='l',lwd=3,col='blue',xlab='x',
        ylab=NA,las=1,cex.lab=1.5)
   title(ylab=TeX('$Linear\\, predictor\\, \\eta (x)$'),line=2.5,cex.lab=1.5)
   # dev.off()
 }
 if(sflag == 2) {
-  # png('LogRegP161001.png',width=16,height=12,units='cm',res=300)
+  # png('LogRegP161001.png',width=16,height=16,units='cm',res=300)
   plot(x,p,type='l',lwd=3,col='blue',xlab='x',ylab=NA,las=1,cex=0.4,cex.lab=1.5)
   title(ylab='Probability of success p',line=2.5,cex.lab=1.5)
   # dev.off()
