991ae045b6f9dab705767758a79b2a9026248043
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-15 09:03:08 +0200

    Add files via upload

--- AIC-LRT.R
+++ AIC-LRT.R
@@ -13,13 +13,14 @@ nu = 1
 yp = dchisq(xp,nu)
 AnAIC = 2
 alphaAIC = 1-pchisq(AnAIC,nu)
+alpha = round(alphaAIC,2)
 library(latex2exp)
-# png('ChiSq1df181119.png',width=16,height=12,units='cm',res=300)
+# png('ChiSq1df181119.png',width=16,height=16,units='cm',res=300)
 plot(xp,yp,type='l',lwd=3,col='blue',xlab='Test statistic q',
      ylab=NA,las=1,cex=0.4,cex.lab=1.5)
 title(ylab=TeX('$\\chi^2(q; \\nu = 1)$'),cex.lab=1.5,line=2.3)
 x1=2; x2=max(xp); y1=dchisq(x1,nu); y2=dchisq(x2,nu); dx=(x2-x1)/50; 
 xn=seq(x1,x2,dx); yn=dchisq(xn,nu); xf=c(x2,x1,xn); yf=c(0,0,yn) 
 polygon(xf,yf,col='red') 
-text(2,0.2,expression(paste(alpha,' = 0.16')),col='red',pos=4,cex=1.5)
+text(2,0.2,bquote(~alpha == .(alpha)),col='red',pos=4,cex=1.5)
 # dev.off()
