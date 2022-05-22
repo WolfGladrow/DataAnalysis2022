627d2bb8b32dd7078cc509f0bab29b8f830a302d
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-05-22 07:11:58 +0200

    Add files via upload

--- PDsPDFsJointPolyPDF.R
+++ PDsPDFsJointPolyPDF.R
@@ -9,6 +9,8 @@ for(m in 1:L) {x = xa[m];
    for(n in 1:L) {y = ya[n]; f[m,n] = alpha*x^2*y+beta*x*y^3}}
 f = f*24/(4*alpha+3*beta)
 print(c(round(max(f),2),'max(f)'))
+# install.packages('plot3D')
+library(plot3D)
 # png('jointPDF3D170219a.png',width=16,height=16,units='cm',res=300)
 persp3D(z=f,main='',clab='PDF',zlab='PDF',breaks = seq(0,7.5,0.5),
         ticktype='detailed',nticks=2,cex.lab=1.5)  # ,clab = 'PDF'
