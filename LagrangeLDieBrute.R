7442c4bb4c031e26ad9c2899d1fc5262a9532463
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-12-17 18:35:20 +0100

    Add files via upload

--- LagrangeLDieBrute.R
+++ LagrangeLDieBrute.R
@@ -1,4 +1,5 @@
 print('file: LagrangeLDieBrute.R')
+print(date())
 # Lagrange multiplier for loaded die: 
 # (1) Lagrange multipliers for unbiased die:
 mu = 3.8    # mean value
@@ -70,37 +71,45 @@ print(c('pj = ',round(pj,4)))
 print('test of constraints:')
 print(c('Constraint 1: sum(pj)    = ',round(sum(pj),4)))
 print(c('Constraint 2: sum(ja*pj) = ',round(sum(ja*pj),4)))
-# [1] '7 = Lagrange multiplier for loaded die (12/2017; 7/2017)'
-# [1] '(1) Lagrange multipliers for unbiased die:'
-# [1] 'L1un = ' '-0.7918'
-# [1] 'L2un = ' '0'      
-# [1] '--------------------------------------------------'
-# [1] '(2) Choose coarse grid around (L1un,L2un):'
-# [1] '--------------------------------------------------'
-# [1] '(3) Calculate sum of squares & find minimum:'
-# [1] '1. index = n = ' '301'            
-# [1] '2. index = m = ' '2037'           
-# [1] 'Mmin1 = '             '2.72457100351743e-09'
-# [1] 'Mmin2 = '             '2.72457100351743e-09'
-# [1] '--------------------------------------------------'
-# [1] '(4) Lagrange parameters (at minimum): 1. approximation'
-# [1] 'L1First = ' '-1.17'     
-# [1] 'L2First =  ' '0.1036'     
-# [1] '--------------------------------------------------'
-# [1] '(5) Remove coarse grid:'
-# [1] '--------------------------------------------------'
-# [1] '(6) Choose fine grid around (L1First,L2First):'
-# [1] '--------------------------------------------------'
-# [1] '(7) Calculate sum of squares & find minimum:'
-# [1] '1. index = n = ' '132'            
-# [1] '2. index = m = ' '94'             
-# [1] 'Mmin1 = '             '1.83982951524073e-11'
-# [1] 'Mmin2 = '             '1.83982951524073e-11'
-# [1] '--------------------------------------------------'
-# [1] '(8) Lagrange parameters (at minimum): 2. approximation'
-# [1] 'Lambda1 = ' '-1.16969'  
-# [1] 'Lambda2 =  ' '0.10353'    
-# [1] 'pj = '  '0.1267' '0.1405' '0.1558' '0.1728' '0.1917' '0.2126'
-# [1] 'test of constraints:'
-# [1] 'Constraint 1: sum(pj)    = ' '1'                          
-# [1] 'Constraint 2: sum(ja*pj) = ' '3.8' 
+print(date())
+# -----------------------------------------------------------------------------
+# Results:
+# "file: LagrangeLDieBrute.R"
+# "Sat Dec 17 18:31:15 2022"
+# "L1un = " "-0.7918"
+# "L2un = " "0"      
+# "--------------------------------------------------"
+# "(2) Choose coarse grid around (L1un,L2un):"
+# "--------------------------------------------------"
+# "(3) Calculate sum of squares & find minimum:"
+# "1. index = n = " "301"            
+# "2. index = m = " "2037"           
+# "Mmin1 = "             "2.72457100351743e-09"
+# "Mmin2 = "             "2.72457100351743e-09"
+# "--------------------------------------------------"
+# "(4) Lagrange parameters (at minimum): 1. approximation"
+# "L1First = " "-1.17"     
+# "L2First =  " "0.1036"     
+# "--------------------------------------------------"
+# "(5) Remove coarse grid:"
+# "--------------------------------------------------"
+# "(6) Choose fine grid around (L1First,L2First):"
+# "--------------------------------------------------"
+# "(7) Calculate sum of squares & find minimum:"
+# "1. index = n = " "132"            
+# "2. index = m = " "94"             
+# "Mmin1 = "             "1.83982951524073e-11"
+# "Mmin2 = "             "1.83982951524073e-11"
+# "--------------------------------------------------"
+# "(8) Lagrange parameters (at minimum): 2. approximation"
+# "Lambda1 = " "-1.16969"  
+# "Lambda2 =  " "0.10353"    
+# "pj = "  "0.1267" "0.1405" "0.1558" "0.1728" "0.1917" "0.2126"
+# "test of constraints:"
+# "Constraint 1: sum(pj)    = " "1"                          
+# "Constraint 2: sum(ja*pj) = " "3.8"                        
+# "Sat Dec 17 18:31:33 2022"
+# -----------------------------------------------------------------------------
+# Remarks:
+# Calculation took 18 s
+# -----------------------------------------------------------------------------
