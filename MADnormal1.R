fa60578d6b977b724fa78e7008232915ee64de69
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-12-17 22:59:54 +0100

    Add files via upload

--- MADnormal1.R
+++ MADnormal1.R
@@ -1,4 +1,5 @@
 print('file: MADnormal1.R')
+print(date())
 # Estimate standard deviation: MAD,normal distribution
 #      MAD = Median Absolute deviation about the Median
 #      Moronna et al. (2006, p.~5)
@@ -17,3 +18,15 @@ xt = 0.7
 text(xt,65,pos=4,paste('mean of estimate = ',as.character(round(meansdest,3))),col='blue',cex=1.5)
 text(xt,55,pos=4,paste('sd of estimate = ',as.character(round(sdsdest,3))),col='blue',cex=1.5)
 # dev.off()
+# -----------------------------------------------------------------------------
+# Results:
+print(c(round(meansdest,4),'meansdest'))
+# "file: MADnormal1.R"
+# "Sat Dec 17 22:54:50 2022"
+# "0.5792"    "meansdest"
+# -----------------------------------------------------------------------------
+# Remarks: 
+# The estimate of the standard deviation using MAD yields a value (0.5792) that is much 
+#   too small (1.0 is the true value for the standard normal PDF). The estimate can be
+#   improved by introducing a scaling factor -> MADN 
+# -----------------------------------------------------------------------------
