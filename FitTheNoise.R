dc07fd0dc28e09074df998c8b8f2771409d92e2e
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-12-17 13:54:07 +0100

    Add files via upload

--- FitTheNoise.R
+++ FitTheNoise.R
@@ -5,13 +5,13 @@ set.seed(1953)
 x = seq(1,4); n = length(x)
 y = -0.3 + 1.3*x+2.1*rnorm(x)
 print('Fit polynomial:')
-out = lm(y ~ x+I(x^2)+I(x^3))
+out = lm(y ~ x+I(x^2)+I(x^3)) # I(): Inhibit Interpretation/Conversion of Objects
 z = summary(out)
 b = z$coefficients[1:n]
 xfit = seq(min(x),max(x),0.01); L = length(xfit)
-yfit = rep(b[1],L)
+yfit = rep(b[1],L) # rep(): Replicate Elements of Vectors and Lists
 for(j in 2:n) yfit = yfit+b[j]*xfit^(j-1)
-outSLR = summary(lm(y ~ x))
+outSLR = summary(lm(y ~ x)) # simple linear regression
 ic = outSLR$coefficients[1]
 slope = outSLR$coefficients[2]
 ySLR = ic+slope*xfit
@@ -22,3 +22,19 @@ abline(0,0,col='green',lty=4)
 lines(xfit,yfit,col='red')
 lines(xfit,ySLR,col='black',lty=2)
 # dev.off()
+# -----------------------------------------------------------------------------
+# Remarks:
+# The command lm(y ~ x+x^2+x^3) would yield an intercept and a slope
+# Coefficients:
+# (Intercept)     x  
+# -1.399        1.680
+# because q = x+x^2+x^3 is used as single predictor.
+#
+# If we want to obtain a polynomial fit instead, we have to inhibit the
+#   interpretation of the polynomial terms by applying I() both to x^2 and x^3 
+# lm(y ~ x+I(x^2)+I(x^3)) 
+# As a result we obtain 4 coefficients (intercept, slopes for x, x^2, and x^3):
+# Coefficients:
+# (Intercept)    x          I(x^2)       I(x^3)  
+# 14.358      -22.297       10.311       -1.326 
+# -----------------------------------------------------------------------------
