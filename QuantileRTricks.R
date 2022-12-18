917b1b37b3d6d91fc764e20d78971af45beaa6c5
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-12-18 14:36:40 +0100

    Add files via upload

--- QuantileRTricks.R
+++ QuantileRTricks.R
@@ -4,7 +4,7 @@ set.seed(1953) # set seed for random number generators
 M = 1e3 # number of Monte Carlo runs
 x = rnorm(M)
 alpha = 1-0.95 # 95% interval
-q = quantile(x,c(alpha/2,1-alpha/2))
+q = quantile(x,c(alpha/2,1-alpha/2)) # ,na.rm='TRUE') 
 qa1 = qnorm(alpha/2)
 qa2 = qnorm(1-alpha/2)
 print(c(round(as.numeric(q[1]),4),'q[1] from random sampling'))
@@ -18,3 +18,16 @@ abline(v=q[2],col='magenta',lty=3)
 abline(v=qa1,col='black',lty=4)
 abline(v=qa2,col='black',lty=4)
 # dev.off()
+# ------------------------------------------------------------
+# Results:
+# "File: QuantileRTricks.R"
+# "-2.0675"      "q[1] from random sampling"
+# "-1.96"        "qa1 analytic"
+#  "1.904"       "q[2] from random sampling"
+#  "1.96"        "qa2 analytic"
+# ------------------------------------------------------------
+# Remarks:
+# The argument na.rm='TRUE' in quantile() is required when
+#   the data x contain missing value (which is not the case
+#   in the example above)
+# ------------------------------------------------------------
