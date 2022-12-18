f966e75a11785aa32d44f8c55fb38f85faaecc17
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-12-18 11:15:34 +0100

    Add files via upload

--- PointEstCorrelation.R
+++ PointEstCorrelation.R
@@ -1,8 +1,9 @@
 print('file: PointEstCorrelation.R')
 # covariance & correlation estimators (7/2020)')
+print(date())
 set.seed(1953)
 source('sampleUcorFct.R')
-r = 0.7 # desired (specified) correlation
+r = 0.7; print(c(r,'desired (specified) correlation')) 
 narr = seq(5,500); L = length(narr)
 covEst1 = numeric(L); covEst2 = numeric(L)
 corEst1 = numeric(L); corEst2 = numeric(L)
@@ -24,3 +25,19 @@ plot(narr,corEst1,log='x',type='p',lwd=4,col='blue',xlab='log(n)',
 points(narr,corEst2,col='red',pch=24,lwd=4,cex=0.6)
 abline(h=r,col='black',lty=2)
 # dev.off()
+# -----------------------------------------------------------------------------
+# Results:
+# "file: PointEstCorrelation.R"
+# "Sun Dec 18 11:01:15 2022"
+# "0.7"         "desired (specified) correlation"
+# "0.7023"      "correlation"
+# "0.0417"      "covariance"
+# -----------------------------------------------------------------------------
+# Remarks:
+# Test of the routine sampleUcorFct(r,n) that generates 2 sample (x and y) of the
+#   same size (n) and that are approximately correlated with a specified 
+#   correlation coefficient (r).
+# For small sample sizes the correlation between the samples can deviate quite a
+#   bit from the specified correlation (r), however, deviations tend to become
+#   smaller with increasing sample size.
+# -----------------------------------------------------------------------------
